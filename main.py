e09413b510da92c55c61f6891ef55b1c52dd60e9
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-05 12:02:54 +0100

    Added menu and set_clock function

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+
+clock = 0, 0, 0
+
+hour_list = [str(i) for i in range(24)]
+
+minute_second_list = [str(i) for i in range(60)]
+
+def clear():
+    os.system("clear")
+
+def menu():
+
+    clear()
+
+    print("1. Display time")
+    print("2. Update time")
+    print("3. Set alarm")
+
+    usr_input = input("Choose an option : ")
+
+    if usr_input == "1":
+        pass
+    elif usr_input == "2":
+        set_clock()
+    elif usr_input == "3":
+        pass
+    else:
+        menu()
+
+def set_clock():
+
+    clear()
+    
+    usr_time_hour = input("Enter hours (0-23) : ")
+    usr_time_minutes = input("Enter minutes (0-59) : ")
+    usr_time_seconds = input("Enter seconds (0-59) : ")
+
+    if usr_time_hour in hour_list and usr_time_minutes in minute_second_list and usr_time_seconds in minute_second_list:
+        return int(usr_time_hour), int(usr_time_minutes), int(usr_time_seconds)
+    else:
+        set_clock()
+
+# Main program
+
+clock = set_clock()
