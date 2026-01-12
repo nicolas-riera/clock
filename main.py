ade6a9212d857a5e47b5b78cad5cb5d0b20a8dce
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-12 15:19:28 +0100

    Removed python version check

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ running = True
 
 if __name__ == "__main__":
 
-    # Verification to check if the user has a new enough version of Python
-    if sys.version_info < (3, 10):
-        print("Your version of Python is too old, please update to Python 3.10+")
-        exit()
-
     clock, pause_offset = set_clock()
 
     interval = time.monotonic()
