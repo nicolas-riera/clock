b11d8669e607c6daa7a7aa9d6572c8b8d9c68eec
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-12 09:21:30 +0100

    Changed variable name to better fit its use

--- src/alarm.py
+++ src/alarm.py
@@ -5,8 +5,8 @@ from src.error import error_messages
 
 # Variables
 
-hour_list = [str(i) for i in range(24)] + [str(i).zfill(2) for i in range(10)]
-minute_second_list = [str(i) for i in range(60)] + [str(i).zfill(2) for i in range(10)]
+HOUR_LIST = [str(i) for i in range(24)] + [str(i).zfill(2) for i in range(10)]
+MINUTE_SECOND_LIST = [str(i) for i in range(60)] + [str(i).zfill(2) for i in range(10)]
 
 # Functions
 
@@ -17,7 +17,7 @@ def set_alarm():
     usr_alarm_minutes = input("Enter minutes (0-59) : ")
     usr_alarm_seconds = input("Enter seconds (0-59) : ")
 
-    if usr_alarm_hour in hour_list and usr_alarm_minutes in minute_second_list and usr_alarm_seconds in minute_second_list:
+    if usr_alarm_hour in HOUR_LIST and usr_alarm_minutes in MINUTE_SECOND_LIST and usr_alarm_seconds in MINUTE_SECOND_LIST:
 
         return int(usr_alarm_hour), int(usr_alarm_minutes), int(usr_alarm_seconds)
     
